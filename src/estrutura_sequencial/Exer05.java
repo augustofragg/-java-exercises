f061f10afa888923c8661cbba33fe6faeb1d03e8
Augusto fragg <augustofragg@gmail.com>
2025-08-10 18:55:16 -0300

    sequential structure, exer 6

--- src/estrutura_sequencial/Exer05.java
+++ src/estrutura_sequencial/Exer05.java
@@ -1,2 +1,29 @@
-package estrutura_sequencial;public class Exer05 {
+package estrutura_sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer05 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int codA= sc.nextInt();
+        int numToolA = sc.nextInt();
+        double unitValueToolA = sc.nextDouble();
+        double totalValueToolA = numToolA * unitValueToolA;
+
+        int codB = sc.nextInt();
+        int numToolB = sc.nextInt();
+        double unitValueToolB = sc.nextDouble();
+        double totalValueToolB = numToolB * unitValueToolB;
+
+        double totalValue = totalValueToolA + totalValueToolB;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f",totalValue);
+
+
+
+        sc.close();
+    }
 }
---
+++ src/estrutura_sequencial/Exer06.java
@@ -0,0 +1,35 @@
+package estrutura_sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer06 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double areaTriangle = (a * c)/2.0;
+
+        double areaCircle = 3.14159 * Math.pow(c,2);
+
+        double areaTrapeze = (a + b) * c / 2.0;
+
+        double areaSquare = Math.pow(b,2);
+
+        double areaRetangle = a * b;
+
+        System.out.printf("TRIANGULO: %.3f\n",areaTriangle);
+        System.out.printf("CIRCULO: %.3f\n",areaCircle);
+        System.out.printf("TRAPEZIO: %.3f\n",areaTrapeze);
+        System.out.printf("QUADRADO: %.3f\n",areaSquare);
+        System.out.printf("RETANGULO: %.3f\n",areaRetangle);
+        
+        
+
+        sc.close();
+    }
+}
