1e8d5ef126203c817ff17249be12f9b8d6055fc5
Augusto fragg <augustofragg@gmail.com>
2025-08-10 18:28:53 -0300

    sequential structure, exer 2

---
+++ src/estrutura_sequencial/Exer02.java
@@ -0,0 +1,21 @@
+package estrutura_sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double pi = 3.14159;
+        double raio = sc.nextDouble();
+
+        double areaCircle = pi * (raio * raio);
+
+        System.out.printf("A=%.4f",areaCircle);
+
+
+
+    }
+}
